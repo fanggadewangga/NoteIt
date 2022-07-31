6a7147df39a1911eb8638f9f19b2d2c7bba9ffed
Fanggadewangga <fanggadewangga@gmail.com>
2022-07-31 19:25:52 +0700

    fix gradle build error

--- app/src/main/java/com/fanggadewangga/noteit/feature_note/presentation/add_edit_notes/AddEditNoteViewModel.kt
+++ app/src/main/java/com/fanggadewangga/noteit/feature_note/presentation/add_edit_notes/AddEditNoteViewModel.kt
@@ -1,8 +1,14 @@
 package com.fanggadewangga.noteit.feature_note.presentation.add_edit_notes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
+import com.fanggadewangga.noteit.feature_note.domain.model.Note
 import com.fanggadewangga.noteit.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +16,52 @@ class AddEditNoteViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
 ): ViewModel() {
 
-    
+    private val _noteTitle = mutableStateOf(NoteTextFieldState(
+        hint = "Enter title..."
+    ))
+    val noteTitle: State<NoteTextFieldState> = _noteTitle
+
+    private val _noteContent= mutableStateOf(NoteTextFieldState(
+        hint = "Enter some contents..."
+    ))
+    val noteContent: State<NoteTextFieldState> = _noteContent
+
+    private val _noteColor = mutableStateOf(Note.noteColor.random().toArgb())
+    val noteColor: State<Int> = _noteColor
+
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun onEvent(event: AddEditNoteEvent) {
+        when(event) {
+            is AddEditNoteEvent.EnteredTitle -> {
+                _noteTitle.value = noteTitle.value.copy(
+                    text = event.value
+                )
+            }
+            is AddEditNoteEvent.ChangeTitleFocus -> {
+                _noteTitle.value = noteTitle.value.copy(
+                    isHintVisible = !event.focusState.isFocused &&
+                            noteTitle.value.text.isBlank()
+                )
+            }
+            is AddEditNoteEvent.EnteredContent -> {
+                _noteContent.value = noteContent.value.copy(
+                    text = event.value
+                )
+            }
+            is AddEditNoteEvent.ChangeContentFocus -> {
+                _noteContent.value = noteContent.value.copy(
+                    isHintVisible = !event.focusState.isFocused &&
+                            noteContent.value.text.isBlank()
+                )
+            }
+            else -> {}
+        }
+    }
+
+    sealed class UiEvent{
+        data class ShowSnackbar(val message: String): UiEvent()
+        object SaveNote: UiEvent()
+    }
 }
